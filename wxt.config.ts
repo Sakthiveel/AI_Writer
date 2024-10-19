781fe008bea4f3ed8c9309ba6ad7dc02bb1e2771
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-10-19 18:36:04 +0530

    chore

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
+// export default defineConfig({
+//   modules: ["@wxt-dev/module-react"],
+// });
 export default defineConfig({
+  runner: {
+    startUrls: ["https://google.com"],
+  },
   manifest: {
     action: {
-      default_title: 'Some Title',
+      default_title: "Some Title",
     },
     web_accessible_resources: [
       {
-        matches: ['*://*.google.com/*'],
-        resources: ['icon/*.png'],
+        matches: ["*://*.google.com/*"],
+        resources: ["icon/*.png"],
       },
     ],
   },
-  modules: ['@wxt-dev/module-react'],
 });
