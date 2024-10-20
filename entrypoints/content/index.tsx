754f874716185d944ff35689ae26a2ac3414dc80
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-10-20 17:14:09 +0530

    feat: ai btn mounting handling updates

--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -2,14 +2,20 @@ import "./style.css";
 import aiIcon from "@/assets/aiIcon.svg";
 import ReactDOM from "react-dom/client";
 
-const AIIconElement = () => (
-  <img
-    src={aiIcon}
-    id="aiIcon"
-    alt="ai_Icon"
-    style={{ width: 32, height: 32 }}
-  />
-);
+const AIIconElement = () => {
+  const clickHandler = () => {
+    console.log("Trigger opening modal");
+  };
+  return (
+    <img
+      src={aiIcon}
+      id="aiIcon"
+      alt="ai_Icon"
+      onClick={clickHandler}
+      style={{ width: 32, height: 32 }}
+    />
+  );
+};
 
 export default defineContentScript({
   matches: ["*://*/*"], // todo: match only linkedin
@@ -23,7 +29,6 @@ export default defineContentScript({
           const nodeList = document.querySelectorAll(
             '[role="dialog"][aria-label="Messaging"][data-view-name="message-overlay-conversation-bubble-item"]'
           );
-
           Array.from(nodeList).forEach((node) => {
             const chatInput = node?.querySelector(
               'div[aria-label="Write a message…"][role="textbox"]'
@@ -32,7 +37,7 @@ export default defineContentScript({
               return;
             }
             elements.set(node, node.id);
-            handleMountAIBtn(ctx, chatInput);
+            handleMountAIBtn(ctx, chatInput, node);
           });
         });
         mutation.removedNodes.forEach((node) => {});
@@ -42,9 +47,13 @@ export default defineContentScript({
     observer.observe(document.body, { childList: true, subtree: true });
   },
 });
+console.log("cool");
 
-async function handleMountAIBtn(ctx, anchorElement) {
+async function handleMountAIBtn(ctx, anchorElement, chatBoxContainer) {
   console.log("me", { anchorElement });
+  let focusListener = () => {};
+  let blurListener = () => {};
+  // let focusOutListener = () => {};
   const ui = await createShadowRootUi(ctx, {
     name: "wxt-react-example",
     position: "overlay",
@@ -59,26 +68,27 @@ async function handleMountAIBtn(ctx, anchorElement) {
       return root;
     },
     onRemove: (elements) => {
+      // todo: need to make sure whether completey recreatign the shadow dom is ok or not
       elements?.unmount();
       // elements?.root.unmount();
       ui.mounted?.unmount();
+      // anchorElement.removeEventListener("focusout", focusOutListener);
       // elements?.wrapper.remove();
     },
   });
-  ui.mount();
-  const focusListener = anchorElement.addEventListener("focus", () => {
-    console.log("mount now");
-    if (!ui.mounted?._internalRoot) {
-      console.log("only", ui.mounted);
-      ui.mount();
-    }
+  if (document.activeElement === anchorElement) {
+    ui.mount();
+  }
+  focusListener = anchorElement.addEventListener("focus", () => {
+    console.log("mount now", chatBoxContainer.id);
+    ui.mount();
   });
-  const blurListener = anchorElement.addEventListener("blur", () => {
-    console.log("remove now");
+
+  blurListener = anchorElement.addEventListener("blur", () => {
+    console.log("remove now", chatBoxContainer.id);
     console.log("type", ui.mounted?._internalRoot);
     ui.remove();
   });
-
   ctx.onInvalidated(() => {
     console.log("ctx invalidated");
     // anchorElement.removeEventListener("focus", focusListener);
